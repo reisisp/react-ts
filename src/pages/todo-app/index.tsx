137bb44c7aaf25484f7f3690b0a05560a5baa43f
Alex <vuyanalex@gmail.com>
2022-11-23 17:30:33 +0300

    fix: fix container position

--- src/pages/todo-app/index.tsx
+++ src/pages/todo-app/index.tsx
@@ -1,7 +1,12 @@
 import TodoWidget from "widgets/todo-widget";
+import CenteredContainer from "shared/centered-container";
 
 const TodoAppPage = () => {
-  return <TodoWidget />;
+  return (
+    <CenteredContainer>
+      <TodoWidget />
+    </CenteredContainer>
+  );
 };
 
 export default TodoAppPage;
--- src/widgets/todo-widget/index.tsx
+++ src/widgets/todo-widget/index.tsx
@@ -2,7 +2,6 @@ import { List } from "antd";
 import TodoCardItem from "entities/todo-card-item";
 import TodoHeader from "entities/todo-header";
 import { useState } from "react";
-import CenteredContainer from "shared/centered-container";
 
 import { taskList } from "./config/task-list";
 import { ITodoItems } from "./config/task-list-type";
@@ -11,21 +10,19 @@ import styles from "./index.module.scss";
 const TodoWidget = () => {
   const [data] = useState<ITodoItems[]>(taskList);
   return (
-    <CenteredContainer>
-      <List
-        size="large"
-        className={styles.itemList}
-        header={<TodoHeader />}
-        footer={<div>Footer</div>}
-        bordered
-        dataSource={data}
-        renderItem={(item) => (
-          <List.Item>
-            <TodoCardItem item={item} />
-          </List.Item>
-        )}
-      />
-    </CenteredContainer>
+    <List
+      size="large"
+      className={styles.itemList}
+      header={<TodoHeader />}
+      footer={<div>Footer</div>}
+      bordered
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item>
+          <TodoCardItem item={item} />
+        </List.Item>
+      )}
+    />
   );
 };
 
