f316fc7d3f3fa2d8e09efb83a9c9a8a55d93c245
Alex <vuyanalex@gmail.com>
2022-11-25 12:23:22 +0300

    fix: fix card-item view

--- src/widgets/todo-widget/ui/card-item/index.tsx
+++ src/widgets/todo-widget/ui/card-item/index.tsx
@@ -1,14 +1,14 @@
 import { FC } from "react";
-import { Typography } from "antd";
+import { Button, Typography } from "antd";
 import { ITodoItem } from "shared/api";
 
 const { Text } = Typography;
 
 const TodoCardItem: FC<{ item: ITodoItem; toggleCompleted: () => void }> = ({ item, toggleCompleted }) => {
   return (
-    <Text delete={!item.completed} onClick={toggleCompleted}>
-      {item.title}
-    </Text>
+    <Button type="link" onClick={toggleCompleted} style={{ paddingLeft: 0 }}>
+      <Text delete={!item.completed}>{item.title}</Text>
+    </Button>
   );
 };
 
