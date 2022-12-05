ff03600fa4961b901303ad6260f9774eef0ef820
Alex <vuyanalex@gmail.com>
2022-12-05 17:22:38 +0300

    fix: small router fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 import { Route, Routes, Navigate } from "react-router";
 import { lazy } from "react";
 
-const TestPage = lazy(() => import("./test"));
 const TodoAppPage = lazy(() => import("./todo-app"));
 const NotFoundPage = lazy(() => import("./not-found"));
 
 export const Routing = () => {
   return (
     <Routes>
-      <Route path="/todo" element={<TodoAppPage />} />
-      <Route path="/test" element={<TestPage />} />
+      <Route path="/" element={<TodoAppPage />} />
       <Route path="/not-found" element={<NotFoundPage />} />
       <Route path="*" element={<Navigate to="/not-found" />} />
     </Routes>
